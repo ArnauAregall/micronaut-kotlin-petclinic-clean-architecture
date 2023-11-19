21357903a37ce955a8b485388a12d7fec35be03f
Arnau Aregall <arnau.are@gmail.com>
2023-11-19 11:42:40 +0100

    Adds Micronaut application class

---
+++ identity-service/src/main/kotlin/tech/aaregall/lab/micronaut/petclinic/identity/IdentityApp.kt
@@ -0,0 +1,7 @@
+package tech.aaregall.lab.micronaut.petclinic.identity
+
+import io.micronaut.runtime.Micronaut.run
+
+fun main(args: Array<String>) {
+    run(*args)
+}
---
+++ identity-service/src/test/kotlin/tech/aaregall/lab/micronaut/petclinic/identity/IdentityAppTest.kt
@@ -0,0 +1,20 @@
+package tech.aaregall.lab.micronaut.petclinic.identity
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class IdentityAppTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
