53ccfe13b18989c24e1b32e4add28cede5d4ddc9
Arnau Aregall <arnau.are@gmail.com>
2023-12-13 09:26:20 +0100

    Pet Service: create Micronaut application

--- pet-service/build.gradle.kts
+++ pet-service/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("io.micronaut.application")
+    id("io.micronaut.aot")
 }
 
 version = "0.1"
@@ -8,11 +9,37 @@ group = "tech.aaregall.lab.petclinic"
 
 dependencies {
     implementation(project(":common"))
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core")
     testImplementation("io.mockk:mockk:${project.properties["mockkVersion"]}")
 }
 
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass.set("tech.aaregall.lab.petclinic.pet.PetApp")
+}
+
+graalvmNative {
+    toolchainDetection = false
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("tech.aaregall.lab.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading.set(false)
+        convertYamlToJava.set(false)
+        precomputeOperations.set(true)
+        cacheEnvironment.set(true)
+        optimizeClassLoading.set(true)
+        deduceEnvironment.set(true)
+        optimizeNetty.set(true)
+    }
 }
---
+++ pet-service/src/main/kotlin/tech/aaregall/lab/petclinic/pet/PetApp.kt
@@ -0,0 +1,7 @@
+package tech.aaregall.lab.petclinic.pet
+
+import io.micronaut.runtime.Micronaut.run
+
+fun main(args: Array<String>) {
+    run(*args)
+}
