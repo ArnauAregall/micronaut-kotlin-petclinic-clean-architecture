aff86d17da2cce3f1a9df9b35feff9dd34c2d399
Arnau Aregall <arnau.are@gmail.com>
2024-01-21 11:46:24 +0100

    Pet Service: adds first name and last name to PetOwner domain model

--- pet-service/src/main/kotlin/tech/aaregall/lab/petclinic/pet/domain/model/PetOwner.kt
+++ pet-service/src/main/kotlin/tech/aaregall/lab/petclinic/pet/domain/model/PetOwner.kt
@@ -3,4 +3,4 @@ package tech.aaregall.lab.petclinic.pet.domain.model
 import java.io.Serializable
 import java.util.UUID
 
-data class PetOwner(val identityId: UUID): Serializable
+data class PetOwner(val identityId: UUID, val firstName: String? = null, val lastName: String? = null): Serializable
