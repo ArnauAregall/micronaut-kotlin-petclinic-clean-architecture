65d53fa5652e77fa7c8f6b82ea382996331c3d8e
Arnau Aregall <arnau.are@gmail.com>
2024-03-25 06:27:21 +0100

    Identity Service: delegates http error handling to CommonHttpExceptionHandler

--- identity-service/src/main/kotlin/tech/aaregall/lab/petclinic/identity/infrastructure/adapters/input/http/error/HttpExceptionHandler.kt
+++ identity-service/src/main/kotlin/tech/aaregall/lab/petclinic/identity/infrastructure/adapters/input/http/error/HttpExceptionHandler.kt
@@ -2,26 +2,18 @@ package tech.aaregall.lab.petclinic.identity.infrastructure.adapters.input.http.
 
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpResponse.badRequest
-import io.micronaut.http.HttpResponse.serverError
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Error
 import io.micronaut.http.server.exceptions.ExceptionHandler
 import jakarta.inject.Singleton
-import org.slf4j.LoggerFactory
+import tech.aaregall.lab.petclinic.common.error.CommonHttpExceptionHandler
 
 @Singleton
 internal class HttpExceptionHandler: ExceptionHandler<Exception, HttpResponse<Any>> {
 
-    private val logger = LoggerFactory.getLogger(this::class.java)
+    private val delegate = CommonHttpExceptionHandler()
 
     @Error(global = true, exception = Exception::class)
-    override fun handle(request: HttpRequest<*>?, exception: Exception?): HttpResponse<Any> {
-        logger.warn("Handling unexpected exception on web infra adapter: ", exception)
-        return when (exception) {
-            is IllegalArgumentException -> badRequest()
-            is IllegalStateException -> HttpResponse.status(HttpStatus.CONFLICT)
-            else -> serverError()
-        }
-    }
+    override fun handle(request: HttpRequest<*>, exception: Exception): HttpResponse<Any> =
+        delegate.handle(request, exception)
+
 }
