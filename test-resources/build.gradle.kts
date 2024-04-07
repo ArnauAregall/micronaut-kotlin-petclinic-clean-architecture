f39743a2bff8336b06c4dbb8ea6d7ec74c8741b2
Arnau Aregall <arnau.are@gmail.com>
2024-04-07 19:44:11 +0200

    Test Resources: implements @CleanDatabase JUnit 5 before each callback

--- test-resources/build.gradle.kts
+++ test-resources/build.gradle.kts
@@ -11,6 +11,8 @@ dependencies {
     implementation("io.micronaut.testresources:micronaut-test-resources-extensions-core")
     implementation("org.testcontainers:testcontainers")
     implementation("org.testcontainers:mockserver")
+    implementation("io.micronaut.test:micronaut-test-junit5")
+    implementation("org.flywaydb:flyway-core")
     implementation("io.rest-assured:kotlin-extensions:${project.properties["restAssuredKotlinExtensionsVersion"]}")
     implementation("com.github.dasniko:testcontainers-keycloak:${project.properties["testcontainersKeycloakVersion"]}")
     api("org.mock-server:mockserver-client-java:${project.properties["testcontainersMockserverVersion"]}")
---
+++ test-resources/src/main/kotlin/tech/aaregall/lab/petclinic/testresources/flyway/CleanDatabase.kt
@@ -0,0 +1,33 @@
+package tech.aaregall.lab.petclinic.testresources.flyway
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.test.extensions.junit5.MicronautJunit5Extension
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(CleanDatabaseCallback::class)
+annotation class CleanDatabase
+
+private class CleanDatabaseCallback: BeforeEachCallback {
+
+    companion object {
+        private val NAMESPACE: Namespace = Namespace.create(MicronautJunit5Extension::class.java)
+
+        private fun getApplicationContext(extensionContext: ExtensionContext): ApplicationContext =
+            extensionContext.root.getStore(NAMESPACE)[ApplicationContext::class.java] as ApplicationContext
+    }
+
+    override fun beforeEach(extensionContext: ExtensionContext) {
+        val flyway = getApplicationContext(extensionContext).getBean(Flyway::class.java)
+        if (!flyway.configuration.isCleanDisabled) {
+            flyway.clean()
+            flyway.migrate()
+        }
+    }
+
+}
